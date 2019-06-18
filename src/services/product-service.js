a83c8df2a9968c3a9a5f5a06c51e0811cd9d1c60
nzmi <ingvr@icloud.com>
2019-06-18 13:30:32 +0500

    product-service WIP

---
+++ src/services/product-service.js
@@ -0,0 +1,30 @@
+export default class ProductService {
+	getProducts() {
+		return [
+			{
+				key: '1',
+				product: 'Товар 1',
+				rowPrice: '2000',
+				fullPrice: '2500'
+			},
+			{
+				key: '2',
+				product: 'Товар 2',
+				rowPrice: '2200',
+				fullPrice: '2700'
+			},
+			{
+				key: '3',
+				product: 'Товар 3',
+				rowPrice: '2200',
+				fullPrice: '2700'
+			},
+			{
+				key: '4',
+				product: 'Товар 4',
+				rowPrice: '2200',
+				fullPrice: '2700'
+			}
+		]
+	}
+}
