4a23845610d558f2f91c42321a02e3bdda5844e7
nzmi <ingvr@icloud.com>
2019-06-18 13:02:21 +0500

    move nav to independent component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { Layout, Menu, Icon, Typography, Table,
+import { Layout, Icon, Typography, Table,
 						Divider, Button } from 'antd';
 import "antd/dist/antd.css";
 
 import StoreHeader from '../store-header';
+import Nav from '../nav';
 
 import './app.scss';
 
@@ -44,30 +45,8 @@ const App = () => {
 		<div className="App">
 			<StoreHeader />
 			<Layout>
-				<Sider width={200} style={{ background: '#fff' }}>
-					<Menu
-						mode="inline"
-						defaultSelectedKeys={['1']}
-						defaultOpenKeys={['sub1']}
-						style={{ height: '100%', borderRight: 0 }}
-					>
-							<Menu.Item key="1">
-								<Icon type="close-circle" />
-								Категория 1
-							</Menu.Item>
-							<Menu.Item key="2">
-								<Icon type="close-circle" />
-								Категория 2
-							</Menu.Item>
-							<Menu.Item key="3">
-								<Icon type="close-circle" />
-								Категория 3
-							</Menu.Item>
-							<Menu.Item key="4">
-								Без категории
-							</Menu.Item>
-					</Menu>
-				</Sider>
+				<Nav />
+
 				<Layout style={{ padding: '0 24px 24px' }}>
 					<Content
 						style={{
