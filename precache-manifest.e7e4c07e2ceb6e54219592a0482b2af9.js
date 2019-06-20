32cedcdc7458d7d482aa340996c8c3793886a502
nzmi <ingvr@icloud.com>
2019-06-20 12:24:09 +0500

    Updates

---
+++ precache-manifest.e7e4c07e2ceb6e54219592a0482b2af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b4fca4ec4d372c53454d53dbd080cb",
+    "url": "/test-store/index.html"
+  },
+  {
+    "revision": "7d8857f02b462f4666f8",
+    "url": "/test-store/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "fdd66b3541e79c044998",
+    "url": "/test-store/static/css/main.137c1147.chunk.css"
+  },
+  {
+    "revision": "7d8857f02b462f4666f8",
+    "url": "/test-store/static/js/2.cd79074b.chunk.js"
+  },
+  {
+    "revision": "fdd66b3541e79c044998",
+    "url": "/test-store/static/js/main.b0ec4b12.chunk.js"
+  },
+  {
+    "revision": "d52e643e62f54bb64446",
+    "url": "/test-store/static/js/runtime~main.50afb37c.js"
+  }
+]);
