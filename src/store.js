3bdee47ac7f73ca5a52b9fa68eac0394fa25fa7c
nzmi <ingvr@icloud.com>
2019-06-28 12:22:42 +0500

    Get data from RESTful api: add middleware redux-thunk #22

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
 
 import reducer from "./reducers";
 
@@ -7,6 +8,6 @@ const logger = createLogger({
   level: "log"
 });
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
