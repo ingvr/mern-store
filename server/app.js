09ced4bc8124f33549f65e0a15c38d734aa03589
nzmi <ingvr@icloud.com>
2019-06-28 12:15:31 +0500

    Build a RESTful api: add get category by id (key) #21

--- server/app.js
+++ server/app.js
@@ -51,6 +51,24 @@ app.post("/api/v1/categories", (req, res) => {
   });
 });
 
+app.get("/api/v1/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  categories.items.map(category => {
+    if (category.key === id) {
+      return res.status(200).send({
+        success: "true",
+        message: "category by id retrieved successfully",
+        category
+      });
+    }
+  });
+  return res.status(404).send({
+    success: "false",
+    message: "category id does not exist"
+  });
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
