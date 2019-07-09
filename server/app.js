ed4f4969c47fcaefad5654eef12e4920be6afe81
nzmi <ingvr@icloud.com>
2019-07-09 10:13:51 +0500

    connect mongoDB: use 'mongoose-auto-increment' for product ids #29

--- server/app.js
+++ server/app.js
@@ -1,22 +1,21 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
-import { categories, products } from "./db";
-import { DATABASE_URI } from "./config";
+import {DATABASE_URI} from './config';
 
-import router from "./routes";
+import router from './routes';
 
-const app = express();
-const mongoose = require("mongoose");
+const app = express(),
+  mongoose = require('mongoose');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
 mongoose.connect(DATABASE_URI);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const PORT = 5000;
 
--- server/models/product.js
+++ server/models/product.js
@@ -1,14 +1,21 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+import {DATABASE_URI} from '../config';
+
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  autoIncrement = require('mongoose-auto-increment');
+
+const connection = mongoose.createConnection(DATABASE_URI);
+autoIncrement.initialize(connection);
 
 const ProductSchema = new Schema(
   {
-    name: { type: String, required: true, max: 100 },
-    rowPrice: { type: Number, required: true },
-    fullPrice: { type: Number, required: true },
-    categoryId: { type: String, required: true }
+    name: {type: String, required: true, max: 100},
+    rowPrice: {type: Number, required: true},
+    fullPrice: {type: Number, required: true},
+    categoryId: {type: String, required: true},
   },
-  { collection: "products" }
+  {collection: 'products'},
 );
 
-module.exports = mongoose.model("Product", ProductSchema);
+ProductSchema.plugin(autoIncrement.plugin, 'Product');
+module.exports = mongoose.model('Product', ProductSchema);
