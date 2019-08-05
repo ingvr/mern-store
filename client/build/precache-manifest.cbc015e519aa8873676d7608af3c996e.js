21334949cb5eac3fcec4d6ad23df51dc09a8e0b6
nzmi <ingvr@icloud.com>
2019-08-05 15:33:52 +0500

    Connect to travis and deploy to heroku: add client/build folder #40
    Modify run command

---
+++ client/build/precache-manifest.cbc015e519aa8873676d7608af3c996e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a834f63f76954ba4209b151aab543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486a2609a41da1ab92dc",
+    "url": "/static/css/2.d19c8617.chunk.css"
+  },
+  {
+    "revision": "24dbe792362d999f94a4",
+    "url": "/static/css/main.753b79e1.chunk.css"
+  },
+  {
+    "revision": "486a2609a41da1ab92dc",
+    "url": "/static/js/2.374098ed.chunk.js"
+  },
+  {
+    "revision": "24dbe792362d999f94a4",
+    "url": "/static/js/main.4e23195f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc757744ad0091ec5edecb1ac45ddedf",
+    "url": "/static/media/authorization.cc757744.jpg"
+  }
+]);
