9d88f4281d4132b80caed184ab3a3573062e0db9
nzmi <ingvr@icloud.com>
2019-08-19 11:17:19 +0500

    Improve project structure: Add client/build for Heroku #41

---
+++ client/build/precache-manifest.81a902e00f02951504bfea2c68b34cb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909c563edf0570fc747a5ec06b92a0ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486a2609a41da1ab92dc",
+    "url": "/static/css/2.d19c8617.chunk.css"
+  },
+  {
+    "revision": "dddba9ddf90e96f448fa",
+    "url": "/static/css/main.753b79e1.chunk.css"
+  },
+  {
+    "revision": "486a2609a41da1ab92dc",
+    "url": "/static/js/2.374098ed.chunk.js"
+  },
+  {
+    "revision": "dddba9ddf90e96f448fa",
+    "url": "/static/js/main.302aac07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc757744ad0091ec5edecb1ac45ddedf",
+    "url": "/static/media/authorization.cc757744.jpg"
+  }
+]);
